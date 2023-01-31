bb6fef11f39725b00e9ab2ca413695bc2a0afae2
IgorSSSena <igorssena22@gmail.com>
2023-01-31 11:25:53 -0300

    Final Icones

---
+++ src/assets/IconCountry/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { IconDefault } from './styles';
+
+const IconCountry = ({icon,height,width}) => {
+
+    const IconImage = require(`../svg/${icon.toLowerCase()}.svg`);
+  
+  return (
+      <IconDefault src={IconImage} alt={icon}  height={height} width ={width}/>
+  )
+}
+
+export default IconCountry
