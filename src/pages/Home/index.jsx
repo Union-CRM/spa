92402afe59e2bfb78331c7179d832a778101db10
Mateus <57973892+Mt-3108@users.noreply.github.com>
2023-02-01 17:53:40 -0300

    Component User list
    Criação do componente User List pela Equipe D.
    Alteração no git ignore, linha: /vscode .

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,54 @@
 import React from "react";
+import UserList from "../../components/UserList";
+import User from "../../components/UserList/User";
 
+{/*Array de Usuários*/}
+const users =[
+  {
+    name:"Bonequinha Cinza",
+    id:2566789,
+    
+  },
+  {
+    name:"Bonequinha Cinza",
+    id:2566789,
+    
+  },
+  {
+    name:"Bonequinha Cinza",
+    id:2566789,
+    
+  },
+  {
+    name:"Bonequinha Cinza",
+    id:2566789,
+    
+  },
+
+]
+
+  
 class Home extends React.Component {
+
+
   render() {
     return (
       <>
+      
         <h1>Let's go!</h1>
+      
+          {/*Lista de Usuários*/}
+          <div>
+           
+          <UserList userCount={"("+ (users.length.toString()) +")"} user={
+              users.map((item,i) =>(<User key={i} name={item.name} id={item.id}/>))}/> 
+        
+          </div>
+
+
       </>
+
+    
     );
   }
 }
