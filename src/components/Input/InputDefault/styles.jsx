a11c2d1ea21b6b5350b2d74ac141b4c499dc42c5
Graziele L. Miranda <lima.graziele@yahoo.com.br>
2023-02-01 15:13:24 -0300

    Input
    criação de input

---
+++ src/components/Input/InputDefault/styles.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  height: 35px;
+  width: 500px;
+  border-radius: 5px;
+  border: 0.3px solid #d9d9d9;
+  background-color: rgba(0, 0, 0, 0);
+  display: block;
+  margin-top: 10px;
+  margin: 20px auto;
+  padding-left: 10px;
+`;
+export const Container = styled.div`
+  box-sizing: border-box;
+  position: center;
+  width: 619px;
+  height: 22vh;
+  background-color: rgba(0, 0, 0, 0);
+`;
