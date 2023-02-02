12b546438197e684efa223b190ce90614ccbd203
Graziele L. Miranda <lima.graziele@yahoo.com.br>
2023-02-02 18:19:25 -0300

    Input Select

---
+++ src/components/Input/SingleSelect/styles.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  box-sizing: border-box;
+  width: auto;
+  height: auto;
+  margin-bottom: 20px;
+  background-color: rgba(0, 0, 0, 0);
+`;
+export const Label = styled.label`
+  margin-top: 10px;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 22px;
+  color: rgba(0, 0, 0, 0.8);
+`;
