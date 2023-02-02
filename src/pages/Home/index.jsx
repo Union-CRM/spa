eeee796d2e48220cd34c002215c7bf8c20c6ed95
Mateus <57973892+Mt-3108@users.noreply.github.com>
2023-02-02 09:47:15 -0300

    Atualização do Componente User List pela Equipe D - Arquivo de Teste

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,33 +1,5 @@
 import React from "react";
-import UserList from "../../components/UserList";
-import User from "../../components/UserList/User";
 
-{/*Array de Usuários*/}
-const users =[
-  {
-    name:"Bonequinha Cinza",
-    id:2566789,
-    
-  },
-  {
-    name:"Bonequinha Cinza",
-    id:2566789,
-    
-  },
-  {
-    name:"Bonequinha Cinza",
-    id:2566789,
-    
-  },
-  {
-    name:"Bonequinha Cinza",
-    id:2566789,
-    
-  },
-
-]
-
-  
 class Home extends React.Component {
 
 
@@ -37,14 +9,7 @@ class Home extends React.Component {
       
         <h1>Let's go!</h1>
       
-          {/*Lista de Usuários*/}
-          <div>
-           
-          <UserList userCount={"("+ (users.length.toString()) +")"} user={
-              users.map((item,i) =>(<User key={i} name={item.name} id={item.id}/>))}/> 
-
-          {/*Funcao necessária para a renderização do componente User List*/}
-          </div>
+    
 
 
       </>
