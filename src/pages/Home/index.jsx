25b791a367f684af3ed0c60b0780b158a48be429
Isabela Bueno <isabueno1011@gmail.com>
2023-02-03 14:52:47 -0300

    Atualização da criação do card group

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,28 @@
 import React from "react";
-<<<<<<< HEAD
+HEAD
 import Card from "../../components/Card";
-=======
+
 import Body from "../../components/Body";
 
->>>>>>> 9a420afe3bd244a064299a7c706330cf8df9ebb6
+
 
 class Home extends React.Component {
   render() {
     return (
       <>
-<<<<<<< HEAD
+< HEAD>
         <h1>Let's go</h1>
         <Card />
-=======
+
         <Body>
           <h1>
             Substitua este H1 pelo seu codigo durante os testes, depois remova-o
           </h1>
         </Body>
->>>>>>> 9a420afe3bd244a064299a7c706330cf8df9ebb6
+</HEAD>
       </>
     );
   }
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> 9a420afe3bd244a064299a7c706330cf8df9ebb6
 export default Home;
