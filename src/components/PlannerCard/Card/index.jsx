a846cb7eb861d9f209f8e0e7a67da2c6dd5a85c8
IgorSSSena <igorssena22@gmail.com>
2023-02-24 14:42:37 -0300

    Co-authored-by: CamilaGVieira <CamilaGVieira@users.noreply.github.com>

--- src/components/PlannerCard/Card/index.jsx
+++ src/components/PlannerCard/Card/index.jsx
@@ -5,26 +5,23 @@ import {Li, P,H2, P1, Img, P2, P3, DivStatus, Horas} from "./styles";
 const Card = (props) => {
     return(
 
-      
             <>
             
-            
-            <Li>
+              <Li>
+                  
+                  <DivStatus />
+                  <Horas>11:00 - 12:00</Horas>
+                  <H2>{props.sector}</H2>
+                  <P>{props.name1}</P>
+                  <P2>{props.name2}</P2>
+                  <P1>{props.email1}</P1>
+                  <P3>{props.email2}</P3>
+                  
                 
-                <DivStatus />
-                <Horas>11:00 - 12:00</Horas>
-                <H2>{props.sector}</H2>
-                <P>{props.name1}</P>
-                <P2>{props.name2}</P2>
-                <P1>{props.email1}</P1>
-                <P3>{props.email2}</P3>
                 
-              
-              
-          </Li>
+              </Li>
           
           </>
-
 
     )
   }
