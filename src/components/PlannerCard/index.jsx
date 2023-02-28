42965017de934fa01ffe3a5819ce45ae7173a7ae
Graziele L. Miranda <lima.graziele@yahoo.com.br>
2023-02-28 12:12:50 -0300

    Update

--- src/components/PlannerCard/index.jsx
+++ src/components/PlannerCard/index.jsx
@@ -1,39 +1,61 @@
 import React from "react";
 import { DivP, H1, DivMain } from "./styles";
 import Card from "./Card";
-
-
-const PlannerCard = (props) =>  {
-    
-    return (
-
-      
+import Body from "../Body";
+const clientes = [
+  {
+    id: 1,
+    name1: "Jornada Digital Itaubers",
+    name2: "Jornada Digital Itaubers",
+    sector: "Finaceiro",
+    email1: "juliana1.costumer@costumer.com.br",
+    email2: "julian.costumer@costumer.com.br",
+  },
+  {
+    id: 2,
+    name1: "Jornada Digital Itaubers",
+    name2: "Jornada Digital Itaubers",
+    sector: "Finaceiro",
+    email1: "juliana.costumer@costumer.com.br",
+    email2: "juliana.costumer@costumer.com.br",
+  },
+  {
+    id: 3,
+    name1: "Jornada Digital Itaubers",
+    name2: "Jornada Digital Itaubers",
+    sector: "Finaceiro",
+    email1: "juliana.costumer@costumer.com.br",
+    email2: "juliana.costumer@costumer.com.br",
+  },
+  {
+    id: 4,
+    name1: "Jornada Digital Itaubers",
+    name2: "Jornada Digital Itaubers",
+    sector: "Finaceiro",
+    email1: "juliana.costumer@costumer.com.br",
+    email2: "juliana.costumer@costumer.com.br",
+  },
+];
+const PlannerCard = () => {
+  return (
+    <Body>
       <DivMain>
         <DivP>
-            <H1>18 - January</H1>
-      
-          
-            {props.array.map((item) => (
-      
-            <Card
+          <H1>18 - January</H1>
 
+          {clientes.map((item) => (
+            <Card
               name1={item.name1}
               email1={item.email1}
               name2={item.name2}
               email2={item.email2}
-              sector = {item.sector}
+              sector={item.sector}
             />
-
-              ))}
-
+          ))}
         </DivP>
       </DivMain>
-      
-
-      
-
-    );
-
-}
+    </Body>
+  );
+};
 
 export default PlannerCard;
--- src/components/SidBar/index.jsx
+++ src/components/SidBar/index.jsx
@@ -72,7 +72,7 @@ class SidBar extends React.Component {
               </Li>
             </Slink>
 
-            <Slink to="/home">
+            <Slink to="/planner">
               <Li>
                 <Icon $mode={this.state.sidBartState}>
                   <IconSystem icon="planner" />
