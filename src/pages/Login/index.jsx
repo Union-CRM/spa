b59ca1e0d1525afadf41dd3c83ee31d5779353ff
mvbrazz <mvbrazz@hotmail.com>
2023-03-15 18:05:49 -0300

    Endpoint comentado

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,30 +1,35 @@
 import React, { useState } from 'react'; // Importar useState para criar estado para email e senha
 import IconSystem from '../../assets/IconSystem';
 import ButtonDefault from '../../assets/Buttons/ButtonDefault';
-import { Container,DivTcs,Content,LogoDiv,Form,DivIcons,Label,Span,Input,ForgotPassword,DivTerms,
-    DivWelcome,TextTerm,LoginBt,AdministratorEnter,DivImgs,DivEmailIcon,DivPassWIcon} from './styles';
+import { Container,DivTcs,Content,LogoDiv,Form,DivIcons,Label,Span,Input,DivTerms,
+    DivWelcome,TextTerm,LoginBt,DivImgs,DivEmailIcon,DivPassWIcon} from './styles';
 import Headline from '../../assets/FontSystem/Headline';
 import axios from 'axios';
 
 function LoginPage() {
+
   const [email, setEmail] = useState(''); // Criar estado para email com o hook useState
   const [password, setPassword] = useState(''); // Criar estado para senha com o hook useState
+
     localStorage.setItem("token","");
+
     async function handleLogin(event) { // Renomear função de teste para handleLogin e adicionar evento de submissão de formulário
-        event.preventDefault(); // Impedir comportamento padrão de submissão do formulário
+      
+      event.preventDefault(); // Impedir comportamento padrão de submissão do formulário
         
 
-        /* O codigo abaixo representa a verificação do login via endpoint 
+        // O codigo abaixo representa a verificação do login via endpoint (FUNCIONANDO)
+        //'http://ec2-18-230-74-206.sa-east-1.compute.amazonaws.com:8081/union/v1/users/login'
+        // Só utilizar quando for apresentar ao Giba.
 
-        // Endpoint AWS = ec2-18-230-74-206.sa-east-1.compute.amazonaws.com
-
-------------------------------------------------------------------------------------------------------------------------
-        
-        if (email !== "" && password !== "" && password.length >= 8) { // Verificar email e senha preenchidos e tamanho mínimo da senha
-            const { data } = await axios.post('http://localhost:8081/union/v1/users/login', {
+        /*
+        if (email !== "" && password !== "") { // Verificar email e senha preenchidos e tamanho mínimo da senha
+            const { data } = await axios.post('http://ec2-18-230-74-206.sa-east-1.compute.amazonaws.com:8081/union/v1/users/login', {
                 email,
                 password,
+  
             }).catch(function (error) {
+             
                 if (error.response) {
                 // el.style.visibility = "visible";
                 setEmail('');
@@ -40,9 +45,9 @@ function LoginPage() {
             localStorage.setItem('token', data.token);
             window.location.href = '/home';
         }*/
-
-        localStorage.setItem('token', "data.token");
-        window.location.href = '/home';
+      
+      localStorage.setItem('token', "data.token");
+      window.location.href = '/home';
 
     }
   
