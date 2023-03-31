13eef8627e60a3bc10077a3514218a73a8eff44c
Mateus <57973892+Mt-3108@users.noreply.github.com>
2023-03-31 11:37:53 -0300

    FTTU002 - Tela Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,61 +4,62 @@ import Donut from "../../components/Admin/AdmGraphicGroup/Donut";
 import ContainerCards from "../../components/Client/ContainerCardClient";
 import { ClientContextProvider } from "../../context/ClientContext";
 
-import {Container,DivSection,DivClient,DivPlanner,Graph} from "./styles";
+import { Container, DivSection, DivClient, DivPlanner,H1Planners,HowManyPlanners, Graph } from "./styles";
 import Grafico from "../../components/Grafico";
 import PageBodyHome from "../../components/Geral/PageBody/PageBodyHome";
-
+import PlannerCard from "../../components/Planner/PlannerCard";
 import { useState } from "react";
-
+import { BigCalender } from "../../components/Planner/Calendar";
+import { PlannerContextProvider } from "../../context/PlannerContext";
+import { usePlannerContext } from "../../hook/usePlannerContext";
 /*const funcClientInfo =()=>{
   const [clientInfo,setClientInfo] = useState("Client Information"); 
   return {clientInfo,setClientInfo};
 }*/
+const dateOfTheDay = new Date();
+
+function PlannerCount(){
+  const { Planner } = usePlannerContext();
+
+  return Planner;
+}
 class Home extends React.Component {
  
   render() {
-    
     return (
       <>
-      <Container>
-      <PageBodyHome>
-     
+        <Container>
+          <PageBodyHome>
+            <DivClient>
+              <ClientContextProvider>
+                <ContainerCards />
+              </ClientContextProvider>
+              <DivPlanner>
+              <H1Planners>Planners </H1Planners>
+              <HowManyPlanners>({PlannerCount.length})</HowManyPlanners>{" "}
+                <PlannerContextProvider>
+              
+                  <PlannerCard date={dateOfTheDay} />
+                </PlannerContextProvider>
+              </DivPlanner>
+            </DivClient>
 
-     
-        <DivClient>
-       
-          <ClientContextProvider>
-         
-          
-         <ContainerCards />
-        
-          </ClientContextProvider>
-       
-          <DivPlanner>
-           <h1>Planner</h1>
-           <h2></h2>
-          </DivPlanner>
-        </DivClient>
-        
+            {/* <ClientCard/>*/}
 
-      {/* <ClientCard/>*/} 
-      
-          <Graph>
-           <Grafico/>
-            <Grafico/>
-        
-          </Graph>    
-    
-      </PageBodyHome>
-      
-      </Container>
-      
+            <Graph>
+              <Grafico />
+              <Grafico />
+            </Graph>
+          </PageBodyHome>
+        </Container>
       </>
     );
   }
 }
 
 export default Home;
-   {/* <Donut></Donut>
+{
+  /* <Donut></Donut>
       <Donut></Donut>
-    <Donut></Donut>*/}
+    <Donut></Donut>*/
+}
