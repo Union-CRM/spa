a513c022ac4dfbe20e9a469654d28cc759112cbe
IgorSSSena <igorssena22@gmail.com>
2023-04-04 14:14:14 -0300

    Atualização com Client buscando do banco

--- src/components/Client/AddEditClient/index.jsx
+++ src/components/Client/AddEditClient/index.jsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect, useRef } from "react";
 import {
   ContainerCentral,
   Container,
-  ContainerChildren,
   PositionTitle,
   H1,
   Input,
@@ -19,14 +18,36 @@ import {
   DivButton,
   ClickButton,
   PositionButtonCancel,
-} from "./styles";
+} from "./styles" ;
+
 import SingleSelect from "../../Geral/Input/SingleSelect";
 import ButtonDefault from "../../../assets/Buttons/ButtonDefault";
 import { TagComponent } from "../../Geral/TagComponent";
-
 import { useClientContext } from "../../../hook/useClientContent";
+import {useFetchRelease} from "../../../hook/useFetchRelease";
+import {useFetchCustomer} from "../../../hook/useFetchCustomer"
+import {useFetchClient} from "../../../hook/useFetchClient"
+import { useFetchRole } from "../../../hook/useFetchRole";
+import { useFetchTag } from "../../../hook/useFetchTag";
 
 const AddEditClient = (props) => {
+  const { client: clientList, setClient: setClientList } = useClientContext();
+
+  const [clientId,setClientId]=useState()
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [customer, setCustomer] = useState({});
+  const [business, setBusiness] = useState("");
+  const [role, setRole] = useState({});
+  const [status, setStatus] = useState();
+  const {releaseList} =useFetchRelease("release");
+  const {customerList} = useFetchCustomer("Customer");
+  const {roleList} = useFetchRole("Role");
+  const {tagList} = useFetchTag("Tag")
+  const [releaseObj, setReleaseObj]=  useState({release_name:"",business_name:""});
+  const [tags, setTags] = useState([]);
+  const {insertClient,updateClient}= useFetchClient();
+  const [flag, setFlag] = useState(false);
   const { setModal, id } = props;
 
   const closeModal = () => {
@@ -51,220 +72,226 @@ const AddEditClient = (props) => {
     return lastId + 1;
   }
 
-  const { client: clientList, setClient: setClientList } = useClientContext();
-
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [customer, setCustomer] = useState();
-  const [business, setBusiness] = useState();
-  const [role, setRole] = useState();
-  const [status, setStatus] = useState();
-  const [release, setRelease] = useState();
-  const [tags, setTags] = useState([]);
-
-  const [flag, setFlag] = useState(false);
-
   useEffect(() => {
     if (props.title === "Edit Client") {
+      const colors=["#836FFF","#00BFFF","#7FFFD4","#00FA9A","#00FF00",
+      "#ADFF2F","#BDB76B","#FFDEAD","#DEB887","#9370DB","#EE82EE",
+      "#FFB6C1","#F08080","#FA8072","#FFA07A","#FFFF00","#7B68EE","#BC8F8F"]
       const client = clientList.filter((item) => item.id === props.id)[0];
+      setClientId(client.id)
       setName(client.client);
       setStatus(client.status);
-      setEmail(client.email);
+      setEmail(client.email)
+      setReleaseObj({
+        id: client.release_id,
+        label: client.textRelease,
+        business_id:  client.business_id,
+        business_name:  client.textBusiness
+      })
       setBusiness(client.textBusiness);
-      setCustomer(client.textCustomer);
-      setRole(client.textRole);
-      setRelease(client.textRelease);
-      setTags(client.tags);
+      setCustomer({id:client.customer_id,label:client.textCustomer});
+      setRole({id:client.role_id,label:client.textRole});
+      setTags(client.tags.map((item)=>({value:item.value,label:item.label,color:colors[Math.floor(Math.random() * (colors.length-1))]})));
     }
-  }, [id]);
-
-  const createClient = () => {
+  }, []);
+  
+  const createClient = () => {  
     const newClient = {
       id: getId(),
       status: status,
       email: email,
       client: name,
-      textRole: role,
-      textCustomer: customer,
-      textBusiness: business,
-      textRelease: release,
+      role_id: role.id,
+      textRole: role.label,
+      customer_id: customer.id,
+      textCustomer: customer.label,
+      business_id:releaseObj.business_id,
+      textBusiness: releaseObj.business_name,
+      release_id: releaseObj.id,
+      textRelease: releaseObj.release_label,
       tags: tags,
     };
 
-    if (name && email && role && customer && business && release) {
-      setClientList([...clientList, newClient]);
+    if (name && email && role.id && customer.id && releaseObj.id) {
+      setClientList([ ...clientList,newClient]);
+      insertClient(newClient);
       setModal(false);
     } else {
       setFlag(true);
     }
   };
+
 
   const editClient = () => {
     const newClient = {
-      id: id,
-      status: status,
+      id:clientId,
       email: email,
       client: name,
-      textRole: role,
-      textCustomer: customer,
-      textBusiness: business,
-      textRelease: release,
+      role_id: role.id,
+      customer_id: customer.id,
+      business_id:releaseObj.business_id,
+      release_id: releaseObj.release_id,
       tags: tags,
     };
-    if (name && email && role && customer && business && release) {
+    
+    if (name && email && role.id && customer.id && releaseObj.id) {
       const noId = clientList.filter((item) => item.id !== id);
       setClientList([...noId, newClient]);
+      updateClient(clientId,newClient);
       setModal(false);
     } else {
       setFlag(true);
     }
   };
 
-  const [userChoice, setUserChoice] = useState("");
 
-  //console.log(userChoice);
+  const handleSelectRelease=(release_id)=>{ 
+    setReleaseObj(releaseList.filter((item)=>item.id===release_id)[0])  
+  }
+
+  const handleSelectCustomer=(customer_id)=>{
+      setCustomer(customerList.filter((c)=>c.id===customer_id)[0])     
+  }
+
+  const handleSelectRole=(role_id)=>{
+    setRole(roleList.filter((c)=>c.id===role_id)[0])     
+  }
 
   return (
     <>
       <ContainerCentral>
         <Container>
-          <ContainerChildren>
-            <PositionTitle>
-              <H1>{props.title} </H1>
+        <PositionTitle>
+            <H1>{props.title} </H1>
 
-              {/*<DivClose>
-              <ClickButton onClick={closeModal}>
-              <Close>Close</Close>
-              </ClickButton>
-            </DivClose>*/}
-            </PositionTitle>
-            <Form>
-              <DivName>
-                <Label>
-                  Client Name
-                  <Input
-                    widthInput={"100% !important"}
-                    placeholder={flag && !name ? "Required field" : ""}
-                    value={name}
-                    required
-                    onChange={(event) => setName(event.target.value)}
-                  />
-                </Label>
-              </DivName>
-
-              <DivEmail>
-                <Label>
-                  Email
-                  <Input
-                    widthInput={"90% !important"}
-                    name={email}
-                    value={email}
-                    placeholder={flag && !email ? "Required field" : ""}
-                    required
-                    onChange={(event) => setEmail(event.target.value)}
-                  />
-                </Label>
-
-                <SingleSelect
-                  set={(role) => setRole(role)}
-                  placeholder={flag && !role ? "Required field" : ""}
-                  name={role}
-                  value={role}
+          </PositionTitle>
+          <Form>
+            <DivName>
+              <Label>
+                Client Name
+                <Input
+                  widthInput={"100% !important"}
+                  placeholder={flag && !name ? "Required field" : ""}
+                  value={name}
                   required
-                  label={"Role"}
-                  sizeSingle={"100%"}
-                  sizeMenu={"100%"}
-                  options={role_mok}
-                  onChange={(choice) => setUserChoice(choice)}
+                  onChange={(event) => setName(event.target.value)}
                 />
-              </DivEmail>
+              </Label>
+            </DivName>
 
-              <DivCustomer>
-                <Label>
-                  Customer
-                  <Input
-                    widthInput={"100% !important"}
-                    name={customer}
-                    value={customer}
-                    placeholder={flag && !customer ? "Required field" : ""}
-                    required
-                    onChange={(event) => setCustomer(event.target.value)}
-                  />
-                </Label>
-              </DivCustomer>
-
-              <DivBusiness>
-                <Label>
-                  Business
-                  <Input
-                    widthInput={"100% !important"}
-                    value={business}
-                    placeholder={flag && !business ? "Required field" : ""}
-                    required
-                    name={business}
-                    onChange={(event) => setBusiness(event.target.value)}
-                  />
-                </Label>
-              </DivBusiness>
-
-              <DivRelease>
-                <SingleSelect
-                  set={(release) => setRelease(release)}
-                  label={"Release Train"}
-                  value={release}
-                  placeholder={flag && !release ? "Required field" : ""}
-                  sizeSingle={"100%"}
+            <DivEmail>
+              <Label>
+                Email
+                <Input
+                  widthInput={"90% !important"}
+                  name={email}
+                  value={email}
+                  placeholder={flag && !email ? "Required field" : ""}
                   required
-                  sizeMenu={"100%"}
-                  options={release_mok}
-                  onChange={(choice) => setUserChoice(choice)}
+                  onChange={(event) => setEmail(event.target.value)}
                 />
-              </DivRelease>
+              </Label>
 
-              <DivTag>
-                <TagComponent
-                  set={(tags) => setTags(tags)}
-                  tags={tags}
-                  label={"Tags"}
-                  placeholder={flag && !tags ? "Required field" : ""}
-                  sizeSingle={"40%"}
-                  required
-                  sizeMenu={"40%"}
-                  options={options}
-                />
-              </DivTag>
+              <SingleSelect
+                key="1"
+                set={(role) => handleSelectRole(role)}
+                placeholder={flag && !role.id ? "Required field" : ""}
+                name={role}
+                value={role.label}
+                required
+                label={"Role"}
+                sizeSingle={"100%"}
+                sizeMenu={"100%"}
+                options={roleList ? roleList:[]}
+              />
+            </DivEmail>
 
-              <DivStatus>
-                <SingleSelect
-                  set={(status) => setStatus(status)}
-                  label={"Status"}
-                  value={status}
-                  placeholder={flag && !status ? "Required field" : ""}
-                  sizeSingle={"40%"}
+            <DivCustomer>
+              <SingleSelect
+                key="2"
+                set={(customer_id) => handleSelectCustomer(customer_id)}
+                label={"Customer"}
+                value={customer.label}
+                placeholder={flag && !customer.id ? "Required field" : ""}
+                sizeSingle={"100%"}
+                required
+                sizeMenu={"100%"}
+                options={customerList ? customerList:[]}
+              />
+            </DivCustomer>
+
+            
+            <DivRelease>
+             <SingleSelect
+                key="3"
+                set={(release_id) => handleSelectRelease(release_id)}
+                label={"Release Train"}
+                value={releaseObj.label}
+                placeholder={flag && !releaseObj.id ? "Required field" : ""}
+                sizeSingle={"100%"}
+                required
+                sizeMenu={"100%"}
+                options={releaseList ? releaseList:[]}
+              />
+            </DivRelease>
+            
+            <DivBusiness>
+              <Label>
+                Business
+                <Input
+                  disabled
+                  widthInput={"100% !important"}
+                  value={releaseObj.business_name}
+                  placeholder={flag && !releaseObj.business ? "Required field" : ""}
                   required
-                  sizeMenu={"40%"}
-                  options={status_mok}
-                  onChange={(choice) => setUserChoice(choice)}
+                  name={business}
                 />
-              </DivStatus>
-            </Form>{" "}
-            <DivButton>
-              <ClickButton onClick={handleSubmit}>
-                <ButtonDefault
-                  type="userSave"
-                  weightFont={"500"}
-                  sizeFont={"18px"}
-                  name={"Save"}
-                />
-              </ClickButton>
-              <PositionButtonCancel onClick={closeModal}>
-                <ButtonDefault type="userCancel" name={"Cancel"} />
-              </PositionButtonCancel>
-            </DivButton>
-          </ContainerChildren>
+              </Label>
+            </DivBusiness>
+
+
+            <DivTag>
+              <TagComponent
+                set={(tags) => setTags(tags)}
+                tags={tags}
+                label={"Tags"}
+                placeholder={flag && !tags ? "Required field" : ""}
+                sizeSingle={"40%"}
+                required
+                sizeMenu={"40%"}
+                options={tagList ? tagList:release_mok}
+              />
+            </DivTag>
+
+            <DivStatus>
+              {false && <SingleSelect
+                set={(status) => setStatus(status)}
+                label={"Status"}
+                value={status}
+                placeholder={flag && !status ? "Required field" : ""}
+                sizeSingle={"40%"}
+                required
+                sizeMenu={"40%"}
+                options={status_mok}
+              />}
+            </DivStatus>
+          </Form>{" "}
+          <DivButton>
+            <ClickButton onClick={handleSubmit}>
+              <ButtonDefault
+                type="userSave"
+                weightFont={"500"}
+                sizeFont={"18px"}
+                name={"Save"}
+              />
+            </ClickButton>
+            <PositionButtonCancel onClick={closeModal}>
+              <ButtonDefault type="userCancel" name={"Cancel"} />
+            </PositionButtonCancel>
+          </DivButton>
         </Container>
       </ContainerCentral>
-    </>
+  </>
   );
 };
 
@@ -292,6 +319,35 @@ const release_mok = [
   { id: 4, value: "Jornada do Cliente", label: "Jornada do Cliente" },
   { id: 5, value: "Dados Onboarding", label: "Dados Onboarding" },
 ];
+
+
+const release_option=[
+  {
+    release_id: 1,
+    release_name: "Pagamento Cartão", 
+    business_id: 1,
+    business_name: "Infraestrutura e Operações TI" 
+  },{
+    
+    release_id: 2,
+    release_name: "Experiencia Digital", 
+    business_id: 1,
+    business_name: "Infraestrutura e Operações TI"
+  },{
+        
+    release_id: 3,
+    release_name: "Inteligencia Comercial", 
+    business_id: 1,
+    business_name: "Infraestrutura e Operações TI"
+  },
+]
+
+const release_mok_2=[
+  {
+    id:1, release_name:"Pagamento Cartão",
+  }
+]
+
 const customers_mok = [
   { id: 1, value: "Itáu", label: "Itaú" },
   { id: 2, value: "Bradesco", label: "Bradesco" },
