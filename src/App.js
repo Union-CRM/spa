444de3d02c45a27a339c0fcf09addb22669c04f5
Graziele L. Miranda <lima.graziele@yahoo.com.br>
2023-04-05 15:17:46 -0300

    Update app.js
    Ataulização app,js para criação do subject

--- src/App.js
+++ src/App.js
@@ -6,67 +6,70 @@ import SubjectPage from "./pages/Subject/index";
 import Client from "./pages/Client";
 import RemarkPage from "./pages/Remark";
 import LoginPage from "./pages/Login/index";
+import { ClientContextProvider } from "./context/ClientContext";
 function App() {
   return (
     <>
-      <Router>
-        <GlobalStyle />
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route
-              path="/home"
-              element={
-                localStorage.getItem("token") != false ? (
-                  <Home />
-                ) : (
-                  <LoginPage />
-                )
-              }
-            />
-            <Route
-              path="/client"
-              element={
-                localStorage.getItem("token") != false ? (
-                  <Client />
-                ) : (
-                  <LoginPage />
-                )
-              }
-            />
-            <Route
-              path="/subject"
-              element={
-                localStorage.getItem("token") != false ? (
-                  <SubjectPage />
-                ) : (
-                  <LoginPage />
-                )
-              }
-            />
-            <Route
-              path="/remark"
-              element={
-                localStorage.getItem("token") != false ? (
-                  <RemarkPage />
-                ) : (
-                  <LoginPage />
-                )
-              }
-            />
-            <Route
-              path="/planner"
-              element={
-                localStorage.getItem("token") != false ? (
-                  <PlannerPage />
-                ) : (
-                  <LoginPage />
-                )
-              }
-            />
-          </Routes>
-        </div>
-      </Router>
+      <ClientContextProvider>
+        <Router>
+          <GlobalStyle />
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<LoginPage />} />
+              <Route
+                path="/home"
+                element={
+                  localStorage.getItem("token") != false ? (
+                    <Home />
+                  ) : (
+                    <LoginPage />
+                  )
+                }
+              />
+              <Route
+                path="/client"
+                element={
+                  localStorage.getItem("token") != false ? (
+                    <Client />
+                  ) : (
+                    <LoginPage />
+                  )
+                }
+              />
+              <Route
+                path="/subject"
+                element={
+                  localStorage.getItem("token") != false ? (
+                    <SubjectPage />
+                  ) : (
+                    <LoginPage />
+                  )
+                }
+              />
+              <Route
+                path="/remark"
+                element={
+                  localStorage.getItem("token") != false ? (
+                    <RemarkPage />
+                  ) : (
+                    <LoginPage />
+                  )
+                }
+              />
+              <Route
+                path="/planner"
+                element={
+                  localStorage.getItem("token") != false ? (
+                    <PlannerPage />
+                  ) : (
+                    <LoginPage />
+                  )
+                }
+              />
+            </Routes>
+          </div>
+        </Router>
+      </ClientContextProvider>
     </>
   );
 }
